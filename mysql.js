dfe7fa5e1ef6a35ff7be117236707959ef8abbe9
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2022-03-22 07:21:46 -0400

    Add files via upload

--- mysql.js
+++ mysql.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "m7az7525jg6ygibs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   user: "mfrsr4fneo8jod2h",
-  password: "ya2oxnpatakbx7jn"
+  password: "ya2oxnpatakbx7jn",
   database: "kuyis9zmrmootcfw"
 });
 
